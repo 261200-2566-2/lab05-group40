9b05c9c90ca91f6aed5b55eb326db294cf81937a
Yotsawat Sukinee <yotsawat2557@gmail.com>
2023-12-05 21:49:49 +0700

    edit one mistake

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
+
 public class Main {
     public static void main(String[] args) {
         RPGCharacter.Mage fireMage = new RPGCharacter.Mage("Mage");
@@ -16,12 +16,7 @@ public class Main {
 
         fireMage.fireball(Boss);
         Boss.Showhp();
-        // Create Warrior character
 
-
-        // Level up Warrior and show status
-
-        // Create Helmet accessory
         RPGAccessory.Helmet steelHelmets = new RPGAccessory.Helmet();
 
         Warrior.equiphelmet(steelHelmets);
